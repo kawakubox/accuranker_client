8d037e784ee97b21541a5fabeb7f3b93f5831358
kawakubox <shigefumi.kawakubo@gmail.com>
2018-04-20 13:54:46 +0900

    Initial commit

---
+++ lib/accuranker_client.rb
@@ -0,0 +1,5 @@
+require "accuranker_client/version"
+
+module AccurankerClient
+  # Your code goes here...
+end
---
+++ spec/accuranker_client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe AccurankerClient do
+  it "has a version number" do
+    expect(AccurankerClient::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
